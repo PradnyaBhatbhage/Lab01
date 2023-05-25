77804b3d3637ee05b9b2a78234c135f90cf9edcc
PradnyaBhatbhage <134581555+PradnyaBhatbhage@users.noreply.github.com>
2023-05-25 13:24:50 +0530

    Update MenuPogram.java

--- MenuPogram.java
+++ MenuPogram.java
@@ -1,26 +1,4 @@
 package org.in;
-/*LAB EXAM
-Course: PG-DAC March 2023 					Date: 30-April-2023
-Duration: 2 hours
-
-1.Create a Java console Application that creates a POJO class representing the details of a Category class that contains
-a.Int catId
-b.String categoryName
-c.String categoryDesc
-d.String categoryImageUrl
-Create a menu driven program that allows the user to enter the value of the Category and store in a any Collection object of your choice.  Duplicate entries should not be permitted.
-Option 1 : allows the user to accept input from the console relating to the above class and store the same in Collection Object
-Option 2 : will allow the user to list all the Category objects stored in the Collection Object.
-Option 3 : Write a Consumer which takes the Collection Object as an argument and displays all categories whose name starts with ‘E’
-
-IMPORTANT :-
-If your surname is Patil then all the Category class created by you should be  class Category_Patil { }. All the objects you create in the above application should have the suffix as objCategory_Patil , objList_Patil, objData_Patil etc.
-*/
-
-/*
-Upload your solution on below google drive link:
-https://forms.gle/BWRearE5tVCiEiDg9
-*/
 
 import java.util.*;
 import java.util.function.Consumer;
